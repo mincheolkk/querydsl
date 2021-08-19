7554094fcdafcb5b02d5524b8ecc185c181e186e
mincheol <kmc04080408@gmail.com>
2021-08-19 23:05:08 +0900

    test : Q타입 동작 확인

--- src/test/java/com/mincheol/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/mincheol/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,38 @@
 package com.mincheol.querydsl;
 
+import com.mincheol.querydsl.entity.Hello;
+import com.mincheol.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(em);
+
+		QHello qHello = QHello.hello;  // Querydsl Q타입 동작 확인
+
+		Hello result = jpaQueryFactory
+				.selectFrom(qHello)
+				.fetchOne();
+
+		Assertions.assertThat(result).isEqualTo(hello);
 	}
 
 }

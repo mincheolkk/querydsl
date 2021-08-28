5ab482595e759fc8cc2120d49cb6b3fcf0265b3f
mincheol <kmc04080408@gmail.com>
2021-08-28 21:32:47 +0900

    feat : 집합 함수, 결과 조회, 정렬, 페이징 추가

--- src/test/java/com/mincheol/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/mincheol/querydsl/QuerydslBasicTest.java
@@ -2,7 +2,10 @@ package com.mincheol.querydsl;
 
 import com.mincheol.querydsl.entity.Member;
 import com.mincheol.querydsl.entity.QMember;
+import com.mincheol.querydsl.entity.QTeam;
 import com.mincheol.querydsl.entity.Team;
+import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,8 +15,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceUnit;
+
+import java.util.List;
 
 import static com.mincheol.querydsl.entity.QMember.*;
+import static com.mincheol.querydsl.entity.QTeam.team;
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -85,7 +92,7 @@ public class QuerydslBasicTest {
         Member findMember = queryFactory
                 .selectFrom(member) // == select(member).from(member)
                 .where(member.username.eq("member1")
-                        .and(member.age.between(10,30)))
+                        .and(member.age.between(10, 30)))
                 .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
@@ -96,9 +103,137 @@ public class QuerydslBasicTest {
         Member findMember = queryFactory
                 .selectFrom(member) // == select(member).from(member)
                 .where(member.username.eq("member1")
-                        ,member.age.eq(10))   // 여러개를 넘기면 다 and 또한 여러 개 중에 null 이 있으면 null 무시함. 동적쿼리에 좋음 .
+                        , member.age.eq(10))   // 여러개를 넘기면 다 and 또한 여러 개 중에 null 이 있으면 null 무시함. 동적쿼리에 좋음 .
                 .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
+
+    @Test
+    public void resultFetch() {
+//        List<Member> fetch = queryFactory
+//                .selectFrom(member)
+//                .fetch();  // member 의 목록을 리스트로 조회
+//
+//        Member fetchOne = queryFactory
+//                .selectFrom(QMember.member)
+//                .fetchOne();
+//
+//        Member fetchFirst = queryFactory
+//                .selectFrom(QMember.member)
+//                .fetchFirst(); // == limit(1).fetchOne()
+
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+        // 페이징 쿼리가 복잡해지면, 데이터를 가져오는 쿼리와 total count 를 가져오는 쿼리가 다를 수도 있음. 성능 때문에.
+        // 복잡하고 성능이 중요한 페이징 쿼리에서는 이걸 쓰면 안 됨. 쿼리 두번을 따로 날리는게 나음.
+
+        results.getTotal();
+        List<Member> content = results.getResults();
+
+        long total = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+    }
+
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순(desc)
+     * 2. 회원 이름 올림차순(asc)
+     * 단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     */
+    @Test
+    public void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+    }
+
+    @Test
+    public void paging1() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())  //orderBy 를 넣어야 페이징이 잘 작동하는지 확인할 수 있음
+                .offset(1)  // 0 부터 시작. 1은 하나를 스킵한다는 뜻.
+                .limit(2)
+                .fetch();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void paging2() {
+        QueryResults<Member> queryResults = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())  //orderBy 를 넣어야 페이징이 잘 작동하는지 확인할 수 있음
+                .offset(1)  // 0 부터 시작. 1은 하나를 스킵한다는 뜻.
+                .limit(2)
+                .fetchResults();
+
+        assertThat(queryResults.getTotal()).isEqualTo(4);
+        assertThat(queryResults.getLimit()).isEqualTo(2);
+        assertThat(queryResults.getOffset()).isEqualTo(1);
+        assertThat(queryResults.getResults().size()).isEqualTo(2);
+    }
+
+    @Test
+    public void aggregation() {
+        List<Tuple> result = queryFactory
+                .select(
+                        member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min()
+                )
+                .from(member)
+                .fetch();
+
+        Tuple tuple = result.get(0);
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+        assertThat(tuple.get(member.age.max())).isEqualTo(40);
+        assertThat(tuple.get(member.age.min())).isEqualTo(10);
+
+        // 데이터 타입이 여러 개 일때 tuple 을 사용함. 실무에선 DTO 로 뽑아냄.
+    }
+
+    /**
+     *  팀의 이름과 각 팀의 평균 연령을 구해라.
+     */
+    @Test
+    public void group() throws Exception {
+        List<Tuple> result = queryFactory
+                .select(team.name, member.age.avg())
+                .from(member)
+                .join(member.team, team)
+                .groupBy(team.name)
+                .fetch();
+
+        Tuple teamA = result.get(0);
+        Tuple teamB = result.get(1);
+
+        assertThat(teamA.get(team.name)).isEqualTo("teamA");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+
+        assertThat(teamB.get(team.name)).isEqualTo("teamB");
+        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+    }
 }
+
